074283c4de782d1e04b8c506c45fb10ec597fee8
Levi <levinard.hugo@connect.qut.edu.au>
2017-05-08 17:49:57 +1000

    Wassup nigga

---
+++ notes/Udemy_WebDev_contents/Exercises/Exercise15_js_while/js.js
@@ -0,0 +1,41 @@
+/* 	1. Print numbers between -10 and 19
+	var num = -16;
+	while (num <= 19) {
+		console.log(num);
+		num++;
+	}
+*/
+
+/*	2. Print all even numbers from 10 to 40
+	var num = 10;
+
+	while (num <= 40) {
+		if (num % 2 == 0) {
+			console.log(num);
+		}
+		num++;
+	}
+*/
+
+/*	3. Print all odd numbers from 300 to 333
+	var number = 300;
+
+	while (number <= 333) {
+		if ((number % 2) != 0) {
+			console.log(number);
+		}
+		number++;
+	}
+*/
+
+/* 4. Print all numbers divisible by 5 AND 3 between 5 and 50
+	var number = 0;
+
+	while (number <= 50) {
+		if ((number % 3 == 0) || (number % 5 == 0)) {
+			console.log(number);
+		}
+
+		number++;
+	}
+*/
