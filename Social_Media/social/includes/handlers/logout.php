1b3f8edc833c9cfaf74847c221fb6e4e7f18b29f
Levi Hugo <levinard.hugo@connect.qut.edu.au>
2018-04-01 21:09:56 +1000

    Removed Social_Media

--- Social_Media/social/includes/handlers/logout.php
+++
@@ -1,5 +0,0 @@
-<?php 
-	session_start();			// makes sure session is started
-	session_destroy();			// destroys session
-	header("Location: ../../register.php");
- ?>
--- notes/Udemy_WebDev_contents/Exercises/Exercise11_javascript_stalker/js.js
+++
@@ -1,6 +0,0 @@
-var fName = prompt("What is your first name?");
-var lName = prompt("What is your last name?");
-var age = prompt("How old are you");
-
-console.log("Hello " + fName + " " + lName);
-console.log("You are " + age + " years old.");
