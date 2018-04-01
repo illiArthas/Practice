65e2f7e321d5cb9cd6b4b77120efdbeb23b104d9
Levi Hugo <levinard.hugo@connect.qut.edu.au>
2018-04-01 21:12:22 +1000

    Removed Udemy_WebDev_contents

--- Udemy_WebDev_contents/Exercises/Exercise12_js_ageCalculator/js.js
+++
@@ -1,7 +0,0 @@
-var age = prompt("Enter your age and find out how many days you've been alive");
-
-var year = 365.25;	// .25 because of leap year
-
-year = year * age;
-
-alert("You've lived " + year);
